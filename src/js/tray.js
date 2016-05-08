0a481e09055b2bc7f5ab72bde6550d554662d8e8
Jakub Goszczurny <szczurmys@o2.pl>
2016-05-08 20:39:49 +0200

    Added menu to tray, with the ability to change the status. Add option "trayAlwaysVisible" (if true, tray is visible on XFCE. On XFCE win.on('minimize' ...), probably not work).

--- src/js/tray.js
+++ src/js/tray.js
@@ -1,18 +1,129 @@
+const AVAILABLE_STATUS_CLASS = "PresencePopup-status--available";
+const BUSY_STATUS_CLASS = "PresencePopup-status--busy";
+const HIDDEN_STATUS_CLASS = "PresencePopup-status--hidden";
+
+var isVisible = true;
+
 try {
-    win.on('minimize', function() {                                              // Get the minimize event
-        this.hide();                                                            // Hide window
-        var tray = new gui.Tray({ title: "Skype", icon: 'skype.png' });        // Show tray and create a tray icon
-        tray.on('click', function() {                                         // Show window and remove tray when clicked
-        win.show();
-        this.remove();                                                   
-        tray = null;
-        });
-    });
-    var __dirname = document.currentScript.src.slice(7, document.currentScript.src.lastIndexOf('.js'));
-    console.log(__dirname + " script successfully loaded");    
+	if(trayAlwaysVisible) {
+		showTray();
+	} else { 
+		showTrayWhenMinimize();
+	}
+
+	var __dirname = document.currentScript.src.slice(7, document.currentScript.src.lastIndexOf('.js'));
+	console.log(__dirname + " script successfully loaded");    
 }
 catch(err) {
-    console.log(err.message + "in" + __dirname);
+	var __dirnameErr = document.currentScript.src.slice(7, document.currentScript.src.lastIndexOf('.js'));
+	console.log(err.message + "in" + __dirnameErr);
 }         
 
+function showTray() {
+	createTray();
+	win.on('minimize', function() {  
+		isVisible=false;     
+	});
+	win.on('restore', function() {  
+		isVisible=true;
+	});
+}       
 
+function showTrayWhenMinimize() {
+	win.on('minimize', function() {  
+		isVisible=false;                                           
+		this.hide();
+		createTray();
+	});
+	win.on('restore', function() {  
+		isVisible=true;
+	});
+}
+
+function createTray() {
+	var tray = new gui.Tray({ title: "Skype", icon: 'skype.png' });
+	tray.on('click', function() {
+		showOrHideWindowWithOptionalRemoveTray(this);
+	});
+	createMenu(tray);
+	return tray;
+}
+
+function showOrHideWindowWithOptionalRemoveTray(tray) {
+	if(!isVisible) {
+		win.show();
+		if(!trayAlwaysVisible) {
+			removeTray(tray);
+		}
+	} else {
+		win.minimize();
+	}
+	isVisible = !isVisible;
+}
+
+function removeTray(tray) {
+	tray.remove();
+	tray = null;
+}
+
+function createMenu(tray) {
+	var menu = new gui.Menu();
+	menu.append(createShowMenuItem());
+	menu.append(new gui.MenuItem({ type: 'separator' }));
+	menu.append(createAvailableMenuItem());
+	menu.append(createBusyMenuItem());
+	menu.append(createNotAvailableMenuItem());
+	menu.append(new gui.MenuItem({ type: 'separator' }));
+	menu.append(createExitMenuItem());
+	tray.menu = menu;
+}
+
+function createShowMenuItem() {
+	var showMenuItem = new gui.MenuItem({ label: 'Show/Hide' });
+	showMenuItem.click = showOrHideWindowWithOptionalRemoveTray;
+	return showMenuItem;
+}
+
+function createAvailableMenuItem() {
+	var availableMenuItem = new gui.MenuItem({ label: 'Available', icon: 'available.png' });
+	availableMenuItem.click = function() { 
+		clickStatus(AVAILABLE_STATUS_CLASS);
+	};
+	return availableMenuItem;
+}
+
+function createBusyMenuItem() {
+	var busyMenuItem = new gui.MenuItem({ label: 'Busy', icon: 'busy.png' });
+	busyMenuItem.click = function() { 
+		clickStatus(BUSY_STATUS_CLASS);
+	};
+	return busyMenuItem;
+}
+
+function createNotAvailableMenuItem() {
+	var notAvailableMenuItem = new gui.MenuItem({ label: 'Not available', icon: 'hidden.png' });
+	notAvailableMenuItem.click = function() { 
+		clickStatus(HIDDEN_STATUS_CLASS);
+	};
+	return notAvailableMenuItem;
+}
+
+function createExitMenuItem() {
+	var exitMenuItem = new gui.MenuItem({ label: 'Exit' });
+	exitMenuItem.click = function() { 
+		win.close(true);
+	};
+	return exitMenuItem;
+}
+
+function clickStatus(statusName) {
+	var skypeFrame = getSkypeWebFrame();
+	var status = getSkypeWebFrame().getElementsByClassName(statusName)
+	if(status.length>0) {
+		status[0].click();
+	}
+}
+
+function getSkypeWebFrame() {
+	return document.getElementById('skypeWebFrame').contentDocument;
+}
