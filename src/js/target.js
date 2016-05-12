898e4b5500dddda13f526e27efbdd89531a61626
Jakub Goszczurny <szczurmys@o2.pl>
2016-05-12 21:06:54 +0200

    Added a feature, which minimizes window when closed by "X"

--- src/js/target.js
+++ src/js/target.js
@@ -4,6 +4,16 @@ try {
         //notify("asd");
         policy.ignore();
     });
+    win.on('close', function () {
+        if(minimizeWhenCloseByX) {
+            win.minimize();
+        } else {
+            this.hide();
+            if (win != null)
+              win.close(true);
+            this.close(true);
+        }
+    });
     var __dirname = document.currentScript.src.slice(7, document.currentScript.src.lastIndexOf('.js'));
     console.log(__dirname + " script successfully loaded");    
 }
