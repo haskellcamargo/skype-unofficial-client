a1052d8ddeca0bc24d9285d6d485bc462782e370
bey0nd_g0dlike <jordan.trahanov@gmail.com>
2016-06-09 15:25:11 +0300

    Hide to tray on 'X' click;

--- src/js/target.js
+++ src/js/target.js
@@ -6,7 +6,7 @@ try {
     });
     win.on('close', function () {
         if(minimizeWhenCloseByX) {
-            win.minimize();
+            win.hide();
         } else {
             this.hide();
             if (win != null)
@@ -15,8 +15,8 @@ try {
         }
     });
     var __dirname = document.currentScript.src.slice(7, document.currentScript.src.lastIndexOf('.js'));
-    console.log(__dirname + " script successfully loaded");    
+    console.log(__dirname + " script successfully loaded");
 }
 catch(err) {
     console.log(err.message + "in" + __dirname);
-}         
+}
